e544c81d29bcb630799e60ce12f84be0f136bfb7
Carlos Zimmerle <cezl@cin.ufpe.br>
2020-06-10 15:31:24 -0300

    feat: add swagger to produce documentation

--- src/main/java/br/ufpe/cin/actuatorservice/ActuatorServiceApplication.java
+++ src/main/java/br/ufpe/cin/actuatorservice/ActuatorServiceApplication.java
@@ -3,8 +3,25 @@ package br.ufpe.cin.actuatorservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ActuatorServiceApplication {
+
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.ant("/hello-world/**")) //replace ant() by any() to capture all endpoints                      
+          .build();                                           
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorServiceApplication.class, args);
