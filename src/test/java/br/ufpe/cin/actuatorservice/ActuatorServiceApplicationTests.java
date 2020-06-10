047af5c96b307382d584df48fdc25fa5831e5eeb
Carlos Zimmerle <cezl@cin.ufpe.br>
2020-06-09 23:18:28 -0300

    feat: add files generated by Spring Initializr

---
+++ src/test/java/br/ufpe/cin/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
